ef95ee5c570c611132ffb611ee2ddd976faa6112
troqy <troqy@example.com>
2025-08-26 04:15:09 +0000

    メイン関数をシンプルなHello Worldに変更し、別ファイルにAxumサーバの実装を追加

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-use axum::{Router, routing::get};
-
-#[tokio::main]
-async fn main() {
-    // Hello Worldと返すハンドラーを定義
-    async fn root_handler() -> String {
-        "Hello Worl 000  55d".to_string()
-    }
-
-    // ルートを定義
-    // "/"を踏むと、上で定義したroot_handlerを実行する
-    let app = Router::new().route("/", get(root_handler));
-
-    // 指定したポートにサーバを開く
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+fn main() {
+    println!("Hello, world!");
 }
